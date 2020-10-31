0cf6610d7ae9ed16f7a84316356d250c70fe4004
Jagadeesh Thiruveedula <34623941+jthiruveedula@users.noreply.github.com>
2020-10-31 17:38:21 +0530

    Create fileContentReplace.py

---
+++ fileContentReplace.py
@@ -0,0 +1,12 @@
+lst=['file1.txt','file2.txt','file3.txt']
+filepath='C:/Users/Jagadeesh/Desktop/python'
+
+for i in lst:
+  with open(filepath+"/"+i,'r') as filer:
+    r=filer.read()
+    r=r.replace("pattern","replacePattern")
+    filer.close()
+  with open(filepath+"\"+i,'w') as filew:
+    filew.write(r)
+    filew.close()
+    
